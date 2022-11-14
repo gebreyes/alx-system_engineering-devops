342f783882ea01ec0f97323ff302d9dae433fd35
Gebreyes Gebeyehu <petros@mtu.edu.et>
2022-11-14 15:53:16 +0300

    creating dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-
+""" Python script to export data in the JSON format."""
 import json
-import requests as r
+import requests
 
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    user = r.get(url + "users").json()
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({usr.get("id"): [{
-            "username": usr.get("username"),
-            "task": e.get("title"),
-            "completed": e.get("completed")
-        } for e in r.get(url + "todos",
-                         params={"userId": usr.get("id")}).json()]
-            for usr in user}, jsonfile)
+if __name__ == "__main__":
+    r = requests.get("https://jsonplaceholder.typicode.com/users/")
+    user = r.json()
+    list_ids = [int(u.get("id")) for u in user]
+    d = {}
+    for i in list_ids:
+        r = requests.get("https://jsonplaceholder.typicode.com/users/",
+                         params={"id": i}).json()
+        user = r[0].get("username")
+        r = requests.get("https://jsonplaceholder.typicode.com/todos/",
+                         params={"userId": i})
+        todos = r.json()
+        l = []
+        dd = {}
+        for t in todos:
+            dd = {"username": user,
+                  "task": t.get("title"),
+                  "completed": t.get("completed")
+                  }
+            l.append(dd)
+        d.update({i: l})
+        with open("todo_all_employees.json", "w") as f:
+            json.dump(d, f)
